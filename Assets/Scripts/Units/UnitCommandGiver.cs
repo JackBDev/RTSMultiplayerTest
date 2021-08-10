2bc6ceb5c6fd3f642949496fc5af67d154904029
Jack Bushnell <jonathan@jonathanbushnell.tech>
2021-08-09 21:57:43 -0400

    Fixed error and created lists of units that players can move.

--- Assets/Scripts/Units/UnitCommandGiver.cs
+++ Assets/Scripts/Units/UnitCommandGiver.cs
@@ -24,11 +24,13 @@ public class UnitCommandGiver : MonoBehaviour
         }
 
         Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-
+        
         if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask))
         {
             return;
         }
+
+        Debug.Log(hit.point);
 
         TryMove(hit.point);
     }
@@ -36,6 +38,7 @@ public class UnitCommandGiver : MonoBehaviour
     //**ERRROR**//
     private void TryMove(Vector3 point)
     {
+        Debug.Log(unitHandler.SelectedUnits.Count);
         foreach(Unit unit in unitHandler.SelectedUnits)
         {
             unit.GetUnitMovement().CmdMovement(point);
--- Assets/Scripts/Units/UnitMovement.cs
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.InputSystem;
 using Mirror;
 
 public class UnitMovement : NetworkBehaviour
